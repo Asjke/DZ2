d98c5d5efaa31faa54da4b75d54245a7583ec936
Evgeniy Filimonov <filimonov-92@list.ru>
2022-03-29 17:52:41 +0300

    Второй коммит

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,85 @@
-<template>
-  <div id="app">
-    
+<template lang="pug">
+  #app.wrapper
+    .main
+      .container
+        h2 Формы
+        Form(@submit.prevent="submitForm")  
+          el-select(
+            v-model="typeBlock.name"
+            placeholder="Выберите из списка"
+          )
+            el-option(
+              v-for="typeBlock.value of typeBlocks"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            )
 
-  </div>
-</template>
+          el-input(
+            type="textarea"
+            :rows="10"
+            v-model="textNews"
+          )
+          el-button.prevent(type="success" native-type="submit") Добавить блок
+      .container
+        h2 Блок новостей
+          
+        span(v-for="item of newsBlock"
+        :key="item"
+        v-html="htmlfoo(item)"
+        )
+  </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-    
+  data () {
+    return {
+      textNews: '',
+      typeBlock: '',
+      typeBlocks: [
+        {label: "Заголовок", value: "title"},
+        {label: "Подзаголовок", value: "subtitle"},
+        {label: "Изображение", value: "image"},
+        {label: "Текст", value: "text"}
+      ],
+      newsBlock: [
+        {}
+      ],
+      newsTypeBlock: []
+    }
+  },
+  methods: {
+    submitForm() {
+      // console.log(this.textNews)
+      // console.log(this.typeBlock)
+  
+      // this.newsTypeBlock.push(this.typeBlock)
+      // console.log(this.newsTypeBlock)
+      // this.textNews = ''
+
+
+    },
+    addNews () {
+
+    },
+    htmlfoo() {
+      // if 
+      // console.log(el.value)
+      // return `<h1>${el}</h1> `
+
+    }
   }
+
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  width: 400px;
+  padding: 0 20px;
+}
+li {
+  list-style: none;
+}
 
 </style>
---
+++ src/components/V-Form.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+  .container
+      h2 Формы
+      Form(@submit.prevent="submitForm")  
+        el-select(
+          v-model="typeBlock"
+          placeholder="Выберите из списка"
+        )
+          el-option(
+            v-for="item of typeBlocks"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          )
+
+        el-input(
+          type="textarea"
+          :rows="10"
+          v-model="textNews"
+        )
+        el-button.prevent(type="success" native-type="submit") Добавить блок
+
+        ul
+          li(v-for="item of newsBlock"
+          :key="item"
+          ) {{ item }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      textNews: '',
+      typeBlock: '',
+      typeBlocks: [
+        {label: "Заголовок", value: "title"},
+        {label: "Подзаголовок", value: "subtitle"},
+        {label: "Изображение", value: "image"},
+        {label: "Текст", value: "text"}
+      ],
+      newsBlock: [
+        
+      ]
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log(this.textNews)
+      console.log(this.typeBlock)
+      this.newsBlock.push(this.textNews)
+      console.log(this.newsBlock)
+      this.textNews = ''
+
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.container {
+  width: 400px;
+  padding: 0 20px;
+}
+
+</style>
